8f67081756efc0b287418b148e0847c4c6ab1fcf
didiwuwu <tinadiwu@gmail.com>
2016-07-17 23:38:49 -0700

    [363. Max Sum of Rectangle No Larger Than K][Accepted]committed by LeetCode Extension

--- 363-Max-Sum-of-Rectangle-No-Larger-Than-K/solution.py
+++ 363-Max-Sum-of-Rectangle-No-Larger-Than-K/solution.py
@@ -14,12 +14,14 @@ class Solution(object):
         for x in range(N):
             sums = [0] * M
             for y in range(x, N):
-                slist, num = [], 0
+                num, slist = 0, []
                 for z in range(M):
                     sums[z] += matrix[z][y] if m > n else matrix[y][z]
                     num += sums[z]
-                    if num <= k: ans = max(ans, num)
-                    i = bisect.bisect_left(slist, num - k)
-                    if i != len(slist): ans = max(ans, num - slist[i])
+                    if num <= k:
+                        ans = max(ans, num)
+                    index = bisect.bisect_left(slist, num - k)
+                    if index != len(slist):
+                        ans = max(ans, num - slist[index])
                     bisect.insort(slist, num)
         return ans or 0
