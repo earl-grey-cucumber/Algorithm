6cb93afdbc30390affbf15d532af39dd86971cf0
didiwuwu <tinadiwu@gmail.com>
2016-07-21 22:05:24 -0700

    [328. Odd Even Linked List][Accepted]committed by LeetCode Extension

---
+++ 328-Odd-Even-Linked-List/solution.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def oddEvenList(self, head):
+        if not head or not head.next:
+            return head
+        cur1, head2, tail1 = head, head.next, None
+        while cur1:
+            if not cur1.next: # 1->2->3, tail1->3
+                tail1 = cur1
+                break
+            cur2 = cur1.next
+            cur1.next = cur2.next
+            if cur2.next:
+                cur2.next = cur2.next.next
+            if not cur1.next: # 1->2->3->4, tail1->3
+                tail1 = cur1
+                break
+            cur1 = cur1.next
+        tail1.next = head2
+        return head
