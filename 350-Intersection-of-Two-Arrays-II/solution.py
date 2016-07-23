0caaa9d7432aca3695f83d1b33fc5c9c9a09439e
didiwuwu <tinadiwu@gmail.com>
2016-07-22 18:20:13 -0700

    [350. Intersection of Two Arrays II][Accepted]committed by LeetCode Extension

---
+++ 350-Intersection-of-Two-Arrays-II/solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        result = []
+        i, j, m, n = 0, 0, len(nums1), len(nums2)
+        nums1 = sorted(nums1)
+        nums2 = sorted(nums2)
+        while i < m and j < n:
+            if nums1[i] == nums2[j]:
+                result.append(nums1[i])
+                i += 1
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
+        return result
+
+                
+        
