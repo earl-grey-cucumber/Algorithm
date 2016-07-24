c56e5038f1857453943eb50cb338a20000dab686
didiwuwu <tinadiwu@gmail.com>
2016-07-24 13:04:49 -0700

    [129. Sum Root to Leaf Numbers][Accepted]committed by LeetCode Extension

---
+++ 129-Sum-Root-to-Leaf-Numbers/solution.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def sumNumbers(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        sums = 0
+        return self.helper(root, sums)
+        
+    def helper(self, cur, sums):
+        if not cur:
+            return 0
+        sums = sums * 10 + cur.val
+        if not cur.left and not cur.right:
+            return sums
+        return self.helper(cur.left, sums) + self.helper(cur.right, sums)
