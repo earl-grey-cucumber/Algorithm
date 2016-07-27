b1c502344dab0f9f7bd988323008bc74ed48d97b
didiwuwu <tinadiwu@gmail.com>
2016-07-26 18:33:58 -0700

    [377. Combination Sum IV][Accepted]committed by LeetCode Extension

---
+++ 377-Combination-Sum-IV/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def combinationSum4(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        dp = [0] * (target + 1)
+        dp[0] = 1
+        for x in range(target + 1):
+            for y in nums:
+                if x + y <= target:
+                    dp[x + y] += dp[x]
+        return dp[target]
