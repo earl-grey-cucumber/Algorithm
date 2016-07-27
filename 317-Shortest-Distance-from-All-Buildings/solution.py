f373dfff35dda9e63556d33b887e25dd8a68d95a
didiwuwu <tinadiwu@gmail.com>
2016-07-27 00:37:34 -0700

    [317. Shortest Distance from All Buildings][Accepted]committed by LeetCode Extension

---
+++ 317-Shortest-Distance-from-All-Buildings/solution.py
@@ -0,0 +1,51 @@
+class Solution(object):
+    def shortestDistance(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        bs = set()
+        m, n = len(grid), len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    bs.add(i * n + j)
+        distance = [[0 for j in range(n)] for i in range(m)]
+        count = [[0 for j in range(n)] for i in range(m)]
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        for b in bs:
+            queue = [b]
+            cur_len = 0
+            visited = [[False for j in range(n)] for i in range(m)]
+            visited[b / n][b % n] = True
+            while queue:
+                size = len(queue)
+                for s in range(size):
+                    cur = queue.pop(0)
+                    i, j = cur / n, cur % n
+                    distance[i][j] += cur_len
+                    count[i][j] += 1
+                    for d in directions:
+                        x, y = i + d[0], j + d[1]
+                        if self.isValid(x, y, m, n, grid, visited):
+                            queue.append(x * n + y)
+                            visited[x][y] = True
+                            
+                cur_len += 1
+        result = sys.maxint
+        for i in range(m):
+             for j in range(n):
+                 if grid[i][j] == 0 and count[i][j] == len(bs) and distance[i][j] < result:
+                     result = distance[i][j]
+        if result == sys.maxint or result == 0:
+            return -1
+        return result
+        
+    def isValid(self, x, y, m, n, grid, visited):
+        if x < 0 or x >= m or y < 0 or y >= n:
+            return False
+        if visited[x][y]:
+            return False
+        if grid[x][y] != 0:
+            return False
+        return True
