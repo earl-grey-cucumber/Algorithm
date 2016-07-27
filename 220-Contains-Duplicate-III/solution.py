c351125592c2a368edcc088a9d951a155e5615f6
didiwuwu <tinadiwu@gmail.com>
2016-07-26 18:25:10 -0700

    [220. Contains Duplicate III][Accepted]committed by LeetCode Extension

--- 220-Contains-Duplicate-III/solution.py
+++ 220-Contains-Duplicate-III/solution.py
@@ -3,27 +3,13 @@ class Solution(object):
     def containsNearbyAlmostDuplicate(self, nums, k, t):
         if k < 0 or t < 0:
             return False
-        window = collections.OrderedDict()
-        for n in nums:
-            if len(window) > k:
-                window.popitem(False)
-            bucket = n if not t else n // t
-            for m in (window.get(bucket - 1), window.get(bucket), window.get(bucket + 1)):
-                if m is not None and abs(n - m) <= t:
+        maps = collections.OrderedDict()
+        for i, num in enumerate(nums):
+            if len(maps) > k:
+                maps.popitem(False)
+            bucket = num if t == 0 else num // t
+            for val in (maps.get(bucket), maps.get(bucket - 1), maps.get(bucket + 1)):
+                if val is not None and abs(val - num) <= t:
                     return True
-            window[bucket] = n
+            maps[bucket] = num
         return False
-        """
-        if k < 0 or t < 0:
-            return False
-	    maps = collections.OrderedDict()
-	    for i, num in enumerate(nums):
-		    if len(maps) > k:
-			    maps.popitem(False)
-		    bucket = num if t == 0 else num / t
-		    if val in (maps.get(bucket), maps.get(bucket - 1), maps.get(bucket + 1)):
-			    if val and abs(val - num) <= t:
-				    return True
-		    maps[bucket] = num
-	    return False
-	    """
