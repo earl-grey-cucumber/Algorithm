efd0e839e5e52f7d6e09777881236f0855a92b49
didiwuwu <tinadiwu@gmail.com>
2016-08-12 11:15:24 -0700

    [338. Counting Bits][Accepted]committed by LeetCode Extension

--- 338-Counting-Bits/solution.py
+++ 338-Counting-Bits/solution.py
@@ -5,9 +5,6 @@ class Solution(object):
         :rtype: List[int]
         """
         dp = [0 for i in range(num + 1)]
-        if num == 0:
-            return dp
-        dp[1] = 1
-        for i in range(2, num + 1):
+        for i in range(1, num + 1):
             dp[i] = dp[i >> 1] + (i & 1)
         return dp
