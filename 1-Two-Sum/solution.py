e6556f2dab0f62e1608240a5c97f1d3ceffa0795
didiwuwu <tinadiwu@gmail.com>
2016-08-12 00:01:55 -0700

    [1. Two Sum][Accepted]committed by LeetCode Extension

---
+++ 1-Two-Sum/solution.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        maps = {}
+        for i, num in enumerate(nums):
+            if target - num in maps:
+                return [maps[target - num], i]
+            maps[num] = i
+        return [-1, -1]
