55ac8411eb21dbf7094d132411abf3bc3ffc7030
didiwuwu <tinadiwu@gmail.com>
2016-08-12 23:21:44 -0700

    [378. Kth Smallest Element in a Sorted Matrix][Accepted]committed by LeetCode Extension

--- 378-Kth-Smallest-Element-in-a-Sorted-Matrix/solution.py
+++ 378-Kth-Smallest-Element-in-a-Sorted-Matrix/solution.py
@@ -1,4 +1,3 @@
-
 class Solution(object):
     def kthSmallest(self, matrix, k):
         """
@@ -6,17 +5,15 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        m, n = len(matrix), len(matrix[0])
         heap = []
-        for j in range(n):
+        for j in range(len(matrix[0])):
             heap.append((matrix[0][j], 0, j))
         heapq.heapify(heap)
         i = 0
         while i < k - 1:
             cur = heapq.heappop(heap)
             x, y = cur[1], cur[2]
-            if x + 1 < m:
+            if x + 1 < len(matrix):
                 heapq.heappush(heap, (matrix[x + 1][y], x + 1, y))
             i += 1
         return heap[0][0]
-        
