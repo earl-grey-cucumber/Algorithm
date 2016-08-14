4240eaaa8f8ce2abce7d94ec224b8b5e4d2b3a49
didiwuwu <tinadiwu@gmail.com>
2016-08-14 00:15:16 -0700

    [337. House Robber III][Accepted]committed by LeetCode Extension

--- 337-House-Robber-III/solution.py
+++ 337-House-Robber-III/solution.py
@@ -7,14 +7,17 @@
 
 class Solution(object):
     def rob(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
         result = [0]
-        self.helper(root, result)
+        def helper(cur, result):
+            if not cur:
+                return [0, 0]
+            left_yes, left_no = helper(cur.left, result)
+            right_yes, right_no = helper(cur.right, result)
+            result[0] = max(left_yes + right_yes, left_no + right_no + cur.val)
+            return result[0], left_yes + right_yes
+        helper(root, result)
         return result[0]
-
-    def helper(self, root, result):
-        if not root:
-            return (0, 0)
-        left_yes, left_no = self.helper(root.left, result)
-        right_yes, right_no = self.helper(root.right, result)
-        result[0] = max(left_no + right_no + root.val, left_yes + right_yes)
-        return result[0], left_yes + right_yes
