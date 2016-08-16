a1664af5f7962e5a2f050084a945553d8a7015b5
didiwuwu <tinadiwu@gmail.com>
2016-08-15 23:01:59 -0700

    [329. Longest Increasing Path in a Matrix][Accepted]committed by LeetCode Extension

--- 329-Longest-Increasing-Path-in-a-Matrix/solution.py
+++ 329-Longest-Increasing-Path-in-a-Matrix/solution.py
@@ -6,23 +6,20 @@ class Solution(object):
         """
         if not matrix:
             return 0
-        m, n = len(matrix), len(matrix[0])
-        max_len = 1
+        def dfs(matrix, i, j, m, n, dp):
+            if dp[i][j] > 0:
+                return dp[i][j]
+            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+            for d in directions:
+                x, y = i + d[0], j + d[1]
+                if 0 <= x < m and 0 <= y < n and matrix[i][j] < matrix[x][y]:
+                    dp[i][j] = max(dp[i][j], dfs(matrix, x, y, m, n, dp))
+            dp[i][j] += 1
+            return dp[i][j]
+            
+        m, n, result = len(matrix), len(matrix[0]), 1
         dp = [[0 for j in range(n)] for i in range(m)]
         for i in range(m):
             for j in range(n):
-                max_len = max(max_len, self.dfs(matrix, i, j, dp))
-        return max_len
-    
-    def dfs(self, matrix, i, j, dp):
-        if dp[i][j]:
-            return dp[i][j]
-        m, n = len(matrix), len(matrix[0])
-        directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
-        for d in directions:
-            x, y = i + d[0], j + d[1]
-            if 0 <= x < m and 0 <= y < n and matrix[i][j] < matrix[x][y]:
-                dp[i][j] = max(dp[i][j], self.dfs(matrix, x, y, dp))
-        dp[i][j] += 1
-        return dp[i][j]
-        
+                result = max(result, dfs(matrix, i, j, m, n, dp))
+        return result
