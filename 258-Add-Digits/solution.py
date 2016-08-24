f61091af5350b9be5828c102a3e1eb4d4846c801
didiwuwu <tinadiwu@gmail.com>
2016-08-23 18:21:24 -0700

    [258. Add Digits][Accepted]committed by LeetCode Extension

---
+++ 258-Add-Digits/solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        if num == 0:
+            return 0
+        return 9 if num % 9 == 0 else num % 9
