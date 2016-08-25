306a2827e348537b2dbef70c4aeac2a1ba88d3af
didiwuwu <tinadiwu@gmail.com>
2016-08-24 23:11:30 -0700

    [366. Find Leaves of Binary Tree][Accepted]committed by LeetCode Extension

---
+++ 366-Find-Leaves-of-Binary-Tree/solution.java
@@ -0,0 +1,14 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> findLeaves(TreeNode root) {
+        
+    }
+}
