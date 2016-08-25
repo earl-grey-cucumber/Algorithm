a8a9c0caa91a7b875a56fea12d8db3369e9d48cc
didiwuwu <tinadiwu@gmail.com>
2016-08-24 21:35:41 -0700

    [286. Walls and Gates][Accepted]committed by LeetCode Extension

--- 286-Walls-and-Gates/solution.py
+++ 286-Walls-and-Gates/solution.py
@@ -1,14 +1,5 @@
 class Solution(object):
     def wallsAndGates(self, rooms):
-        def dfs(rooms, i, j, m, n):
-            INF = 2147483647
-            directions = [[1,0],[-1,0],[0,1],[0,-1]]
-            for d in directions:
-                x, y = i + d[0], j + d[1]
-                if 0 <= x < m and 0 <= y < n and rooms[x][y] > rooms[i][j] + 1:
-                    rooms[x][y] = rooms[i][j] + 1
-                    dfs(rooms, x, y, m, n)
-                    
         INF = 2147483647
         if len(rooms) == 0:
             return
@@ -16,4 +7,13 @@ class Solution(object):
         for i in range(m):
             for j in range(n):
                 if rooms[i][j] == 0:
-                    dfs(rooms, i, j, m, n)
+                    self.dfs(rooms, i, j, m, n)
+        
+    def dfs(self, rooms, i, j, m, n):
+        directions = [[1,0],[-1,0],[0,1],[0,-1]]
+        for d in directions:
+            x, y = i + d[0], j + d[1]
+            if 0 <= x < m and 0 <= y < n and rooms[x][y] > rooms[i][j] + 1:
+                rooms[x][y] = rooms[i][j] + 1
+                self.dfs(rooms, x, y, m, n)
+                    
