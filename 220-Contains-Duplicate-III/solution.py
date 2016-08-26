0eb29b8953b988663f54cf9cc83a1026bba513d7
didiwuwu <tinadiwu@gmail.com>
2016-08-25 22:04:55 -0700

    [220. Contains Duplicate III][Accepted]committed by LeetCode Extension

--- 220-Contains-Duplicate-III/solution.py
+++ 220-Contains-Duplicate-III/solution.py
@@ -1,15 +1,20 @@
-import collections
 class Solution(object):
     def containsNearbyAlmostDuplicate(self, nums, k, t):
+        """
+        :type nums: List[int]
+        :type k: int
+        :type t: int
+        :rtype: bool
+        """
         if k < 0 or t < 0:
             return False
-        maps = collections.OrderedDict()
-        for i, num in enumerate(nums):
-            if len(maps) > k:
-                maps.popitem(False)
-            bucket = num if t == 0 else num // t
-            for val in (maps.get(bucket), maps.get(bucket - 1), maps.get(bucket + 1)):
-                if val is not None and abs(val - num) <= t:
+        window = collections.OrderedDict()
+        for n in nums:
+            if len(window) > k:
+                window.popitem(False)               
+            bucket = n if not t else n // t
+            for m in (window.get(bucket - 1), window.get(bucket), window.get(bucket + 1)):
+                if m is not None and abs(n - m) <= t:
                     return True
-            maps[bucket] = num
+            window[bucket] = n
         return False
