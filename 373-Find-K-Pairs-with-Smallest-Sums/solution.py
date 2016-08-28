16702253ea2f67421c6abf825c3778c3cc2d622d
didiwuwu <tinadiwu@gmail.com>
2016-08-27 23:00:27 -0700

    [373. Find K Pairs with Smallest Sums][Accepted]committed by LeetCode Extension

--- 373-Find-K-Pairs-with-Smallest-Sums/solution.py
+++ 373-Find-K-Pairs-with-Smallest-Sums/solution.py
@@ -1,25 +1,15 @@
 class Solution(object):
     def kSmallestPairs(self, nums1, nums2, k):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :type k: int
-        :rtype: List[List[int]]
-        """
-        heap = []
-        count = 0
-        for i in range(len(nums1)):
-            for j in range(len(nums2)):
-                count += 1
-                if count <= k:
-                    heapq.heappush(heap, [-(nums1[i] + nums2[j]), nums1[i], nums2[j]])
-                else:
-                    top = -heap[0][0]
-                    if nums1[i] + nums2[j] < top:
-                        heapq.heappop(heap)
-                        heapq.heappush(heap, [-(nums1[i] + nums2[j]), nums1[i], nums2[j]])
-        result = []
-        while heap:
-            cur = heapq.heappop(heap)
-            result.append([cur[1], cur[2]])
-        return result[::-1]
+        queue = []
+        def push(i, j):
+            if i < len(nums1) and j < len(nums2):
+                heapq.heappush(queue, [nums1[i] + nums2[j], i, j])
+        push(0, 0)
+        pairs = []
+        while queue and len(pairs) < k:
+            _, i, j = heapq.heappop(queue)
+            pairs.append([nums1[i], nums2[j]])
+            push(i, j + 1)
+            if j == 0:
+                push(i + 1, 0)
+        return pairs
