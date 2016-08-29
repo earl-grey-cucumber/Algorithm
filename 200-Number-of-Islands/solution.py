f10bf76f129308932f6186872d85ef2bea74c16b
didiwuwu <tinadiwu@gmail.com>
2016-08-29 00:07:55 -0700

    [200. Number of Islands][Accepted]committed by LeetCode Extension

--- 200-Number-of-Islands/solution.py
+++ 200-Number-of-Islands/solution.py
@@ -1,43 +1,23 @@
-class union_find(object):
-    def __init__(self, size):
-        self.f = [-1] * size
-    
-    def get_father(self, x):
-        if self.f[x] == x:
-            return x
-        self.f[x] = self.get_father(self.f[x])
-        return self.f[x]
-
-    def merge(self, x, y):
-        if self.f[x] == -1:
-            return False
-        if self.f[y] == -1:
-            return False
-        x = self.get_father(x)
-        y = self.get_father(y)
-        if x == y:
-            return False
-        else:
-            self.f[x] = y
-            return True
-        
 class Solution(object):
     def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
         if not grid:
             return 0
         m, n, count = len(grid), len(grid[0]), 0
-        uf = union_find(m * n)
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == '0':
-                    continue
-                cur = i * n + j
-                if uf.f[cur] == -1:
-                    uf.f[cur] = cur
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j, m, n)
                     count += 1
-                directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-                for d in directions:
-                    x, y = i + d[0], j + d[1]
-                    if 0 <= x < m and  0 <= y < n and grid[x][y] == '1' and uf.merge(cur, x * n + y):
-                        count -= 1
         return count
+    
+    def dfs(self, grid, x, y, m, n):
+        if x < 0 or x >= m or y < 0 or y >= n or grid[x][y] == "0":
+            return
+        grid[x][y] = "0"
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        for d in directions:
+            self.dfs(grid, x + d[0], y + d[1], m, n)
