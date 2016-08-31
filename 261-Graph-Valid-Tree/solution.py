e0f8ad76b67511f0c1c3cf62bbed3566cff3c62c
didiwuwu <tinadiwu@gmail.com>
2016-08-30 23:19:44 -0700

    [261. Graph Valid Tree][Accepted]committed by LeetCode Extension

--- 261-Graph-Valid-Tree/solution.py
+++ 261-Graph-Valid-Tree/solution.py
@@ -1,29 +1,13 @@
-class UnionFind(object):
+class union_find(object):
     def __init__(self, n):
         self.f = range(n)
         self.count = n
-    
-    def find_father(self, x):
+
+    def find(self, x):
         if self.f[x] == x:
             return x
-        self.f[x] = self.find_father(self.f[x])
+        self.f[x] = self.find(self.f[x])
         return self.f[x]
-        
-    def merge(self, x, y):
-        x = self.find_father(x)
-        y = self.find_father(y)
-        if x != y:
-            self.f[x] = y
-            self.count -= 1
-    
-class Solution(object):
-    def validTree(self, n, edges):
-        uf = UnionFind(n)
-        for x, y in edges:
-            if uf.find_father(x) == uf.find_father(y):
-                return False
-            uf.merge(x, y)
-        return uf.count == 1
 
     def merge(self, x, y):
         x = self.find(x)
@@ -31,4 +15,18 @@ class Solution(object):
         if x != y:
             self.f[x] = y
             self.count -= 1
-   
+
+class Solution(object):
+    def validTree(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: bool
+        """
+        uf = union_find(n)
+        for x, y in edges:
+            if uf.find(x) == uf.find(y):
+                return False
+            uf.merge(x, y)
+        return uf.count == 1
+        
