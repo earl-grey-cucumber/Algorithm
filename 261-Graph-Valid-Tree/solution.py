36a5425167f6da65c4c4fc07be301a23ea510e79
didiwuwu <tinadiwu@gmail.com>
2016-08-30 23:37:39 -0700

    [261. Graph Valid Tree][Accepted]committed by LeetCode Extension

--- 261-Graph-Valid-Tree/solution.py
+++ 261-Graph-Valid-Tree/solution.py
@@ -1,32 +1,13 @@
-class union_find(object):
-    def __init__(self, n):
-        self.f = range(n)
-        self.count = n
-
-    def find(self, x):
-        if self.f[x] == x:
-            return x
-        self.f[x] = self.find(self.f[x])
-        return self.f[x]
-
-    def merge(self, x, y):
-        x = self.find(x)
-        y = self.find(y)
-        if x != y:
-            self.f[x] = y
-            self.count -= 1
-
 class Solution(object):
     def validTree(self, n, edges):
-        """
-        :type n: int
-        :type edges: List[List[int]]
-        :rtype: bool
-        """
-        uf = union_find(n)
-        for x, y in edges:
-            if uf.find(x) == uf.find(y):
-                return False
-            uf.merge(x, y)
-        return uf.count == 1
+        if len(edges) != n - 1:
+            return False
+        neighbors = {i: [] for i in range(n)}
+        for v, w in edges:
+            neighbors[v] += w,
+            neighbors[w] += v,
+        def visit(v):
+            map(visit, neighbors.pop(v, []))
+        visit(0)
+        return not neighbors
         
