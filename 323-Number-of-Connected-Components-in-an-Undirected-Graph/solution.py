88458fdba0621e6b2cf77d630c334fd5ff631b25
didiwuwu <tinadiwu@gmail.com>
2016-08-30 23:05:32 -0700

    [323. Number of Connected Components in an Undirected Graph][Accepted]committed by LeetCode Extension

--- 323-Number-of-Connected-Components-in-an-Undirected-Graph/solution.py
+++ 323-Number-of-Connected-Components-in-an-Undirected-Graph/solution.py
@@ -1,31 +1,33 @@
 class union_find(object):
-        def __init__(self, n):
-            self.f = range(n)
-            self.count = n
-
-        def find(self, x):
-            if self.f[x] == x:
-                return x
-            self.f[x] = self.find(self.f[x])
-            return self.f[x]
-
-        def merge(self, x, y):
-            x = self.find(x)
-            y = self.find(y)
-            if x != y:
-                self.f[x] = y
-                self.count -= 1
+    def __init__(self, size):
+        self.f = [i for i in range(size)]
+        self.count = size
+        
+    def findFather(self, i):
+        if self.f[i] == i:
+            return i
+        self.f[i] = self.findFather(self.f[i])
+        return self.f[i]
+        
+    def merge(self, x, y):
+        if self.f[x] == -1:
+            return False
+        if self.f[y] == -1:
+            return False
+        x = self.findFather(x)
+        y = self.findFather(y)
+        if x == y:
+            return False
+        else:
+            self.f[y] = x
+            return True
 
 class Solution(object):
     def countComponents(self, n, edges):
-        """
-        :type n: int
-        :type edges: List[List[int]]
-        :rtype: int
-        """
         uf = union_find(n)
-        for x, y in edges:
-            uf.merge(x, y)
+        for edge in edges:
+            x, y = edge[0], edge[1]
+            if uf.merge(x, y):
+                uf.count -= 1
         return uf.count
-            
             
