c699ca3e75689ac86068e81e92085234d6dac649
didiwuwu <tinadiwu@gmail.com>
2016-08-30 22:45:52 -0700

    [305. Number of Islands II][Accepted]committed by LeetCode Extension

--- 305-Number-of-Islands-II/solution.py
+++ 305-Number-of-Islands-II/solution.py
@@ -1,47 +1,42 @@
 class union_find(object):
     def __init__(self, size):
         self.f = [-1] * size
-        
-    def findFather(self, i):
-        if self.f[i] == i:
-            return i
-        self.f[i] = self.findFather(self.f[i])
-        return self.f[i]
-        
+    
+    def get_father(self, x):
+        if self.f[x] == x:
+            return x
+        self.f[x] = self.get_father(self.f[x])
+        return self.f[x]
+
     def merge(self, x, y):
         if self.f[x] == -1:
             return False
         if self.f[y] == -1:
             return False
-        x = self.findFather(x)
-        y = self.findFather(y)
+        x = self.get_father(x)
+        y = self.get_father(y)
         if x == y:
             return False
         else:
-            self.f[y] = x
+            self.f[x] = y
             return True
-            
+
 class Solution(object):
-    def isValid(self, x, y, m, n):
-        return 0 <= x < m and 0 <= y < n
-        
     def numIslands2(self, m, n, positions):
-        if not positions:
-            return []
-        result = []
-        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        uf = union_find(m * n)
         count = 0
-        for p in positions:
-            cur = p[0] * n + p[1]
-            if uf.f[cur] == -1:
-                count += 1
-                uf.f[cur] = cur
+        result = []
+        uf = union_find(m * n)
+        for position in positions:
+            i, j = position[0], position[1]
+            cur = i * n + j
+            uf.f[cur] = cur
+            count += 1
+            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
             for d in directions:
-                x, y = p[0] + d[0], p[1] + d[1]
-                if self.isValid(x, y, m, n) and uf.merge(cur, x * n + y):
+                x, y = i + d[0], j + d[1]
+                if 0 <= x < m and  0 <= y < n and uf.f[x * n + y] != -1 and uf.merge(cur, x * n + y):
                     count -= 1
             result.append(count)
         return result
 
- 
+        
