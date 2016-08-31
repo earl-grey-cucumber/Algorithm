84b51017cb56d53ced5dfd4ffebc7e9618dfb780
didiwuwu <tinadiwu@gmail.com>
2016-08-30 23:38:44 -0700

    [261. Graph Valid Tree][Accepted]committed by LeetCode Extension

--- 261-Graph-Valid-Tree/solution.py
+++ 261-Graph-Valid-Tree/solution.py
@@ -6,8 +6,13 @@ class Solution(object):
         for v, w in edges:
             neighbors[v] += w,
             neighbors[w] += v,
+        """
         def visit(v):
             map(visit, neighbors.pop(v, []))
         visit(0)
+        """
+        queue = collections.deque([0])
+        while queue:
+            queue.extend(neighbors.pop(queue.popleft(), []))
         return not neighbors
         
