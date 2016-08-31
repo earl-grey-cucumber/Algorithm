531380f2d9d5f60a9ec31c3cebed2506902ca0b6
didiwuwu <tinadiwu@gmail.com>
2016-08-30 22:34:03 -0700

    [200. Number of Islands][Accepted]committed by LeetCode Extension

--- 200-Number-of-Islands/solution.py
+++ 200-Number-of-Islands/solution.py
@@ -1,4 +1,3 @@
-"""
 class UnionFind(object):
     def __init__(self, size):
         self.f = [-1] * size
@@ -12,13 +11,13 @@ class UnionFind(object):
     def merge(self, x, y):
         if self.f[x] == -1 or self.f[y] == -1:
             return False
-        fx, fy = self.get_f(x), self.get_f(y)
-        if fx == fy:
+        x, y = self.get_f(x), self.get_f(y)
+        if x == y:
             return False
         else:
             self.f[x] = y
             return True
-
+"""
 class Solution(object):
     def numIslands(self, grid):
         if not grid:
@@ -38,36 +37,13 @@ class Solution(object):
                         if 0 <= x < m and 0 <= y < n and grid[x][y] == '1' and uf.merge(i * n + j, x * n + y):
                             count -= 1
         return count
-"""
-class union_find(object):
-    def __init__(self, size):
-        self.f = [-1] * size
-    
-    def get_father(self, x):
-        if self.f[x] == x:
-            return x
-        self.f[x] = self.get_father(self.f[x])
-        return self.f[x]
-
-    def merge(self, x, y):
-        if self.f[x] == -1:
-            return False
-        if self.f[y] == -1:
-            return False
-        x = self.get_father(x)
-        y = self.get_father(y)
-        if x == y:
-            return False
-        else:
-            self.f[x] = y
-            return True
-        
+"""        
 class Solution(object):
     def numIslands(self, grid):
         if not grid:
             return 0
         m, n, count = len(grid), len(grid[0]), 0
-        uf = union_find(m * n)
+        uf = UnionFind(m * n)
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == '0':
