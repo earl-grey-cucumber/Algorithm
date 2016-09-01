b42ae4cfd9742209d19bec45f1babf308ea73dcc
didiwuwu <tinadiwu@gmail.com>
2016-09-01 10:11:41 -0700

    [370. Range Addition][Accepted]committed by LeetCode Extension

--- 370-Range-Addition/solution.py
+++ 370-Range-Addition/solution.py
@@ -5,13 +5,14 @@ class Solution(object):
         :type updates: List[List[int]]
         :rtype: List[int]
         """
-        nums = [0] * (length + 1)
+        nums = [0] * length
         for update in updates:
             s, e, change = update[0], update[1], update[2]
             nums[s] += change
-            nums[e + 1] -= change
+            if e + 1 < length:
+                nums[e + 1] -= change
         sum = 0
-        for i in range(len(nums) - 1):
+        for i in range(len(nums)):
             sum += nums[i]
             nums[i] = sum
-        return nums[:-1]
+        return nums
