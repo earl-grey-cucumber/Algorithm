98355644b7e91f65f999377c841c9cee7a9721c4
didiwuwu <tinadiwu@gmail.com>
2016-08-31 23:30:08 -0700

    [173. Binary Search Tree Iterator][Accepted]committed by LeetCode Extension

--- 173-Binary-Search-Tree-Iterator/solution.py
+++ 173-Binary-Search-Tree-Iterator/solution.py
@@ -7,9 +7,6 @@
 
 class BSTIterator(object):
     def __init__(self, root):
-        """
-        :type root: TreeNode
-        """
         self.stack = []
         self.cur = root
         while self.cur:
@@ -17,23 +14,13 @@ class BSTIterator(object):
             self.cur = self.cur.left
 
     def hasNext(self):
-        """
-        :rtype: bool
-        """
         return self.cur or self.stack
 
     def next(self):
-        """
-        :rtype: int
-        """
         self.cur = self.stack.pop()
-        nextVal = self.cur.val
+        val = self.cur.val
         self.cur = self.cur.right
         while self.cur:
             self.stack.append(self.cur)
             self.cur = self.cur.left
-        return nextVal
-
-# Your BSTIterator will be called like this:
-# i, v = BSTIterator(root), []
-# while i.hasNext(): v.append(i.next())
+        return val
