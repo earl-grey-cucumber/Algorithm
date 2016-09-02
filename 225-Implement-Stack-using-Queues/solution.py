ddaaf41c704877a5735c89afd0dbd1f1719b0d30
didiwuwu <tinadiwu@gmail.com>
2016-09-01 17:14:29 -0700

    [225. Implement Stack using Queues][Accepted]committed by LeetCode Extension

--- 225-Implement-Stack-using-Queues/solution.py
+++ 225-Implement-Stack-using-Queues/solution.py
@@ -1,34 +1,38 @@
 class Stack(object):
     def __init__(self):
-        """
-        initialize your data structure here.
-        """
+        self.q = []
+        
+    def push(self, x):
+        size = len(self.q)
+        self.q.append(x)
+        while size > 0:
+            self.q.append(self.q.pop(0))
+            size -= 1
+            
+    def pop(self):
+        return self.q.pop(0)
+
+    def top(self):
+        return self.q[0]
+        
+    def empty(self):
+        return len(self.q) == 0
+    """
+    def __init__(self):
         self.q1, self.q2 = [], []
 
     def push(self, x):
-        """
-        :type x: int
-        :rtype: nothing
-        """
         self.q2.append(x)
         while self.q1:
             self.q2.append(self.q1.pop(0))
         self.q1, self.q2 = self.q2, self.q1
 
     def pop(self):
-        """
-        :rtype: nothing
-        """
         self.q1.pop(0)
 
     def top(self):
-        """
-        :rtype: int
-        """
         return self.q1[0]
 
     def empty(self):
-        """
-        :rtype: bool
-        """
         return len(self.q1) == 0
+    """
