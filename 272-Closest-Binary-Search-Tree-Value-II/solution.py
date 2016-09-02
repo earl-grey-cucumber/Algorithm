a3d0a55120e02740081946134b7fe2202397c3ea
didiwuwu <tinadiwu@gmail.com>
2016-09-01 22:20:20 -0700

    [272. Closest Binary Search Tree Value II][Accepted]committed by LeetCode Extension

--- 272-Closest-Binary-Search-Tree-Value-II/solution.py
+++ 272-Closest-Binary-Search-Tree-Value-II/solution.py
@@ -7,44 +7,40 @@
 
 class Solution(object):
     def closestKValues(self, root, target, k):
-        """
-        :type root: TreeNode
-        :type target: float
-        :type k: int
-        :rtype: List[int]
-        """
-        if not root:
-            return []
-        left, right, result = [], [], []
-        self.helper1(root, target, left)
-        self.helper2(root, target, right)
-        while k > 0:
-            if not left:
-                result.append(right.pop())
-            elif not right:
-                result.append(left.pop())
-            elif target - left[-1] < right[-1] - target:
-                result.append(left.pop())
+        def helper1(cur, target, s1):
+            if not cur:
+                return
+            helper1(cur.left, target, s1)
+            if cur.val > target:
+                return
+            s1.append(cur.val)
+            helper1(cur.right, target, s1)
+        def helper2(cur, target, s2):
+            if not cur:
+                return
+            helper2(cur.right, target, s2)
+            if cur.val <= target:
+                return
+            s2.append(cur.val)
+            helper2(cur.left, target, s2)
+        stack1, stack2 = [], []
+        helper1(root, target, stack1)
+        helper2(root, target, stack2)
+        i, j, l = 0, len(stack1) - 1, len(stack2) - 1
+        result = []
+        while i < k:
+            if j < 0:
+                result.append(stack2[l])
+                l -= 1
+            elif l < 0:
+                result.append(stack1[j])
+                j -= 1
+            elif target - stack1[j] <= stack2[l] - target:
+                result.append(stack1[j])
+                j -= 1
             else:
-                result.append(right.pop())
-            k -= 1
+                result.append(stack2[l])
+                l -= 1
+            i += 1
         return result
-    
-    def helper1(self, cur, target, left):
-        if not cur:
-            return
-        self.helper1(cur.left, target, left)
-        if cur.val > target:
-            return
-        left.append(cur.val)
-        self.helper1(cur.right, target, left)
-        
-    def helper2(self, cur, target, right):
-        if not cur:
-            return
-        self.helper2(cur.right, target, right)
-        if cur.val <= target:
-            return
-        right.append(cur.val)
-        self.helper2(cur.left, target, right)
                 
