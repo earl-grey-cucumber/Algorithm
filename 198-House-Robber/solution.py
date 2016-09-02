8e61f00964424426e5c9a1d73fba4b46c752f74a
didiwuwu <tinadiwu@gmail.com>
2016-09-01 23:20:26 -0700

    [198. House Robber][Accepted]committed by LeetCode Extension

--- 198-House-Robber/solution.py
+++ 198-House-Robber/solution.py
@@ -9,11 +9,11 @@ class Solution(object):
             return 0
         if n == 1:
             return nums[0]
-        t0, t1, t2 = nums[0], max(nums[0], nums[1]), 0
+        t0, t1 = nums[0], max(nums[0], nums[1])
         for i in range(2, n):
-            t2 = max(t1, t0 + nums[i])
-            t0 = t1
-            t1 = t2
+            temp = t1
+            t1 = max(t1, t0 + nums[i])
+            t0 = temp
         return t1
             
             
