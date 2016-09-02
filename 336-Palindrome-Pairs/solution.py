1761d6a20deef73c68ebfe8269b7da34680d55c6
didiwuwu <tinadiwu@gmail.com>
2016-09-02 10:30:26 -0700

    [336. Palindrome Pairs][Accepted]committed by LeetCode Extension

--- 336-Palindrome-Pairs/solution.py
+++ 336-Palindrome-Pairs/solution.py
@@ -4,17 +4,17 @@ class Solution(object):
         :type words: List[str]
         :rtype: List[List[int]]
         """
+        mapping = {}
         result = []
-        maps = {}
-        for i, word in enumerate(words):
-            maps[word] = i
+        for i in range(len(words)):
+            mapping[words[i]] = i
         for i in range(len(words)):
             word = words[i]
             for j in range(len(word) + 1):
-                pre = word[:j]
-                suf = word[j:]
-                if suf == suf[::-1] and pre[::-1] in maps and maps[pre[::-1]] != i:
-                    result.append([i, maps[pre[::-1]]])
-                if j > 0 and pre == pre[::-1] and suf[::-1] in maps and maps[suf[::-1]] != i:
-                    result.append([maps[suf[::-1]], i])
+                prefix = word[:j]
+                suffix = word[j:]
+                if suffix == suffix[::-1] and prefix[::-1] in mapping and mapping[prefix[::-1]] != i:
+                    result.append([i, mapping[prefix[::-1]]])
+                if j > 0 and prefix == prefix[::-1] and suffix[::-1] in mapping and mapping[suffix[::-1]] != i:
+                    result.append([mapping[suffix[::-1]], i])
         return result
