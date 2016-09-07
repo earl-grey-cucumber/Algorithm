c55c8785c764d29b08eb01cf89b1790420faa7cb
didiwuwu <tinadiwu@gmail.com>
2016-09-07 11:11:24 -0700

    [388. Longest Absolute File Path][Accepted]committed by LeetCode Extension

--- 388-Longest-Absolute-File-Path/solution.py
+++ 388-Longest-Absolute-File-Path/solution.py
@@ -1,13 +1,13 @@
-from collections import defaultdict
 class Solution(object):
     def lengthLongestPath(self, input):
         maxlen = 0
-        pathlen = {0: 0}
-        for line in input.splitlines():
-            name = line.lstrip('\t')
-            depth = len(line) - len(name)
-            if '.' in name:
-                maxlen = max(maxlen, pathlen[depth] + len(name))
+        lines = input.splitlines()
+        maps = {0: 0}
+        for line in lines:
+            content = line.lstrip('\t')
+            depth = len(line) - len(content)
+            if '.' in content:
+                maxlen = max(maxlen, maps[depth] + len(content))
             else:
-                pathlen[depth + 1] = pathlen[depth] + len(name) + 1
+                maps[depth + 1] = maps[depth] + len(content) + 1
         return maxlen
