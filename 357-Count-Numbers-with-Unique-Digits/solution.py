cf87f89bbbd37499a7d5a2301656b1862cebdbe1
didiwuwu <tinadiwu@gmail.com>
2016-09-07 11:38:27 -0700

    [357. Count Numbers with Unique Digits][Accepted]committed by LeetCode Extension

--- 357-Count-Numbers-with-Unique-Digits/solution.py
+++ 357-Count-Numbers-with-Unique-Digits/solution.py
@@ -1,21 +1,17 @@
 class Solution(object):
     def countNumbersWithUniqueDigits(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+        if n < 0:
+            return 0
         if n == 0:
             return 1
-        dp = [0 for i in range(n + 1)]
-        dp[0] = 1
-        dp[1] = 9
+        dp = [1, 9] + [0] * (9 - 1)
+        result = dp[0] + dp[1]
         if n == 1:
-            return 10
-        result = 10
+            return result
         for i in range(2, n + 1):
             if 9 - i + 2 > 1:
                 dp[i] = dp[i - 1] * (9 - i + 2)
-                result += dp[i]
             else:
                 break
+            result += dp[i]
         return result
