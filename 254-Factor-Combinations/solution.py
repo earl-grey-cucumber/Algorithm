d28d50a68ec4070283232273d8443e4239b7f7de
didiwuwu <tinadiwu@gmail.com>
2016-09-08 09:43:44 -0700

    [254. Factor Combinations][Accepted]committed by LeetCode Extension

--- 254-Factor-Combinations/solution.py
+++ 254-Factor-Combinations/solution.py
@@ -1,5 +1,16 @@
 class Solution(object):
     def getFactors(self, n):
+        todo, combis = [(n, 2, [])], []
+        while todo:
+            n, i, combi = todo.pop()
+            while i * i <= n:
+                if n % i == 0:
+                    combis += combi + [i, n/i],
+                    todo += (n/i, i, combi+[i]),
+                i += 1
+        return combis
+        
+        """
         def factor(n, i, combi, combis):
             while i * i <= n:
                 if n % i == 0:
@@ -8,6 +19,8 @@ class Solution(object):
                 i += 1
             return combis
         return factor(n, 2, [], [])
+        """
+        
         """
         def dfs(index, result, path, cur):
             if cur == 1:
