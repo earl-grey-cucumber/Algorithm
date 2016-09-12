9374da7bd2065d887cdef7671e42920e37b52e0d
didiwuwu <tinadiwu@gmail.com>
2016-09-11 23:07:30 -0700

    [300. Longest Increasing Subsequence][Accepted]committed by LeetCode Extension

--- 300-Longest-Increasing-Subsequence/solution.py
+++ 300-Longest-Increasing-Subsequence/solution.py
@@ -1,26 +1,5 @@
 class Solution(object):
     def lengthOfLIS(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        """
-        n = len(nums)
-        dp = []
-        for i in range(n):
-            l, h = 0, len(dp) - 1
-            while l <= h:
-                mid = l + (h - l) / 2
-                if dp[mid] < nums[i]:
-                    low = mid + 1
-                else:
-                    high = mid - 1
-            if l < len(dp):
-                dp[l] = num[i]
-            else:
-                dp.append(nums[i])
-        
-        """
         size = len(nums)
         dp = []
         for x in range(size):
@@ -36,3 +15,5 @@ class Solution(object):
             else:
                 dp.append(nums[x])
         return len(dp)
+
+
