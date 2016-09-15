677be06d16a3dfec01f5cc422257f91759c80172
didiwuwu <tinadiwu@gmail.com>
2016-09-14 18:05:34 -0700

    [209. Minimum Size Subarray Sum][Accepted]committed by LeetCode Extension

--- 209-Minimum-Size-Subarray-Sum/solution.py
+++ 209-Minimum-Size-Subarray-Sum/solution.py
@@ -5,15 +5,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        i, j = 0, 0
-        cur_sum = 0
+        def helper(low, sums, target):
+            high, cand = len(sums) - 1, len(sums)
+            while low <= high:
+                mid = (low + high) / 2
+                if sums[mid] >= target:
+                    cand = min(cand, mid)
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            return cand
         min_len = sys.maxint
-        while j < len(nums):
-            cur_sum += nums[j]
-            while i <= j and cur_sum >= s:
-                min_len = min(min_len, j - i + 1)
-                cur_sum -= nums[i]
-                i += 1
-            j += 1
+        n = len(nums)
+        sums = [0 for i in range(n + 1)]
+        for i in range(n):
+            sums[i + 1] = sums[i] + nums[i]
+        for i in range(n + 1):
+            index = helper(i, sums, s + sums[i])
+            if i < index <= n:
+                min_len = min(min_len, index - i)
         return min_len if min_len != sys.maxint else 0
+        
             
