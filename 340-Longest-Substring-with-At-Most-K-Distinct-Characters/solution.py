d959352ab1584a715bb8566bff5060e3fe09615c
didiwuwu <tinadiwu@gmail.com>
2016-09-14 23:23:28 -0700

    [340. Longest Substring with At Most K Distinct Characters][Accepted]committed by LeetCode Extension

---
+++ 340-Longest-Substring-with-At-Most-K-Distinct-Characters/solution.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+class Solution(object):
+    def lengthOfLongestSubstringKDistinct(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        if not s or k == 0:
+            return 0
+        i, j, n, result = 0, 0, len(s), 0
+        maps = defaultdict(int)
+        while j < n:
+            if s[j] in maps or len(maps) < k:
+                maps[s[j]] += 1
+            else: 
+                maps[s[j]] = 1
+                result = max(result, j - i)
+                while i < j and len(maps) > k:
+                    maps[s[i]] -= 1
+                    if maps[s[i]] == 0:
+                        del maps[s[i]]
+                    i += 1
+            j += 1
+        result = max(result, n - i)
+        return result
