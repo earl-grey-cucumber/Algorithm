fdc9e2ed83e2fcc89fff131aca1f5bdc2ab4b487
didiwuwu <tinadiwu@gmail.com>
2016-09-17 16:43:29 -0700

    [302. Smallest Rectangle Enclosing Black Pixels][Accepted]committed by LeetCode Extension

--- 302-Smallest-Rectangle-Enclosing-Black-Pixels/solution.py
+++ 302-Smallest-Rectangle-Enclosing-Black-Pixels/solution.py
@@ -6,6 +6,18 @@ class Solution(object):
         :type y: int
         :rtype: int
         """
+        def helper_col1(image, i, j, top, bottom, opt):
+            left = j
+            while i <= j:
+                k, mid = top, (i + j) / 2
+                while k < bottom and image[k][mid] == '0':
+                    k += 1
+                if k < bottom:
+                    left = min(left, mid)
+                    j = mid - 1
+                else:
+                    i = mid + 1
+            return left
         def helper_col(image, i, j, top, bottom, opt):
             while i != j:
                 k, mid = top, (i + j) / 2
@@ -16,6 +28,7 @@ class Solution(object):
                 else:
                     i = mid + 1
             return i
+            
         def helper_row(image, i, j, left, right, opt):
             while i != j:
                 k, mid = left, (i + j) / 2
@@ -27,7 +40,7 @@ class Solution(object):
                     i = mid + 1
             return i
         m, n = len(image), len(image[0])
-        left = helper_col(image, 0, y, 0, m, True)
+        left = helper_col1(image, 0, y, 0, m, True)
         right = helper_col(image, y + 1, n, 0, m, False)
         top = helper_row(image, 0, x, left, right, True)
         bottom = helper_row(image, x + 1, m, left, right, False)
