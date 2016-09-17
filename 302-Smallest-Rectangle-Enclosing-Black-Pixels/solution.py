a4395f130d5fb38faa1070e96e221e1fb419461e
didiwuwu <tinadiwu@gmail.com>
2016-09-17 16:51:44 -0700

    [302. Smallest Rectangle Enclosing Black Pixels][Accepted]committed by LeetCode Extension

--- 302-Smallest-Rectangle-Enclosing-Black-Pixels/solution.py
+++ 302-Smallest-Rectangle-Enclosing-Black-Pixels/solution.py
@@ -9,9 +9,9 @@ class Solution(object):
         def helper_col(image, i, j, top, bottom, opt, cand):
             while i <= j:
                 k, mid = top, (i + j) / 2
-                while k < bottom and image[k][mid] == '0':
+                while k <= bottom and image[k][mid] == '0':
                     k += 1
-                if k < bottom:
+                if k <= bottom:
                     if opt:
                         cand = min(cand, mid)
                         j = mid - 1
@@ -25,19 +25,27 @@ class Solution(object):
                          j = mid - 1
             return cand
             
-        def helper_row(image, i, j, left, right, opt):
-            while i != j:
+        def helper_row(image, i, j, left, right, opt, cand):
+            while i <= j:
                 k, mid = left, (i + j) / 2
-                while k < right and image[mid][k] == '0':
+                while k <= right and image[mid][k] == '0':
                     k += 1
-                if (k < right) == opt:
-                    j = mid
+                if k <= right:
+                    if opt:
+                        cand = min(cand, mid)
+                        j = mid - 1
+                    else:
+                        cand = max(cand, mid)
+                        i = mid + 1
                 else:
-                    i = mid + 1
-            return i
+                    if opt:
+                        i = mid + 1
+                    else:
+                         j = mid - 1
+            return cand
         m, n = len(image), len(image[0])
-        left = helper_col(image, 0, y, 0, m, True, y)
-        right = helper_col(image, y, n - 1, 0, m, False, y)
-        top = helper_row(image, 0, x, left, right + 1, True)
-        bottom = helper_row(image, x + 1, m, left, right + 1, False)
-        return (right - left + 1) * (bottom - top)
+        left = helper_col(image, 0, y, 0, m - 1, True, y)
+        right = helper_col(image, y, n - 1, 0, m - 1, False, y)
+        top = helper_row(image, 0, x, left, right, True, x)
+        bottom = helper_row(image, x, m - 1, left, right, False, x)
+        return (right - left + 1) * (bottom - top + 1)
