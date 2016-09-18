607ca47affc1f26ceb64410a35b16d66d0c4e0bd
didiwuwu <tinadiwu@gmail.com>
2016-09-17 22:02:53 -0700

    [361. Bomb Enemy][Accepted]committed by LeetCode Extension

--- 361-Bomb-Enemy/solution.py
+++ 361-Bomb-Enemy/solution.py
@@ -1,29 +1,26 @@
 class Solution(object):
     def maxKilledEnemies(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
-        """
         if not grid:
             return 0
         m, n = len(grid), len(grid[0])
-        result, rowhit, colhit = 0, 0, [0] * n
-        for i, row in enumerate(grid):
-            for j, cell in enumerate(row):
-                if j == 0 or row[j - 1] == 'W':
+        result, rowhit = 0, 0
+        colhit = [0] * n
+        for i in range(m):
+            for j in range(n):
+                if j == 0 or grid[i][j - 1] == 'W':
                     rowhit = 0
                     k = j
-                    while k < n and row[k] != 'W':
-                        rowhit += row[k] == 'E'
+                    while k < n and grid[i][k] != 'W':
+                        rowhit += (grid[i][k] == 'E')
                         k += 1
                 if i == 0 or grid[i - 1][j] == 'W':
-                    colhit[j] == 0
+                    colhit[j] = 0
                     k = i
                     while k < m and grid[k][j] != 'W':
-                        colhit[j] += grid[k][j] == 'E'
+
+                        colhit[j] += (grid[k][j] == 'E')
                         k += 1
-                if cell == '0':
+                if grid[i][j] == '0':
                     result = max(result, rowhit + colhit[j])
         return result
 
@@ -49,7 +46,7 @@ class Solution(object):
                 if cell == '0':
                     result = max(result, rowhits + colhits[j])
         return result
-        
+        """ 
         """
         if not grid:
             return 0
