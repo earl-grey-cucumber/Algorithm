8339aa004a748a2c10a2e28bbe550d218629aa37
didiwuwu <tinadiwu@gmail.com>
2016-09-17 21:52:24 -0700

    [361. Bomb Enemy][Accepted]committed by LeetCode Extension

--- 361-Bomb-Enemy/solution.py
+++ 361-Bomb-Enemy/solution.py
@@ -9,25 +9,24 @@ class Solution(object):
             return 0
         m, n = len(grid), len(grid[0])
         result, rowhit, colhit = 0, 0, [0] * n
-        for i in range(m):
-            for j in range(n):
-                if j == 0 or grid[i][j - 1] == 'W':
+        for i, row in enumerate(grid):
+            for j, cell in enumerate(row):
+                if j == 0 or row[j - 1] == 'W':
                     rowhit = 0
                     k = j
-                    while k < n and grid[i][k] != 'W':
-                        #if grid[i][k] == 'E':
-                        rowhit += grid[i][k] == 'E'
+                    while k < n and row[k] != 'W':
+                        rowhit += row[k] == 'E'
                         k += 1
                 if i == 0 or grid[i - 1][j] == 'W':
                     colhit[j] == 0
                     k = i
                     while k < m and grid[k][j] != 'W':
-                        #if grid[k][j] == 'E':
                         colhit[j] += grid[k][j] == 'E'
                         k += 1
-                if grid[i][j] == '0':
+                if cell == '0':
                     result = max(result, rowhit + colhit[j])
         return result
+
         """
         if not grid: return 0
         m, n = len(grid), len(grid[0])
@@ -50,6 +49,7 @@ class Solution(object):
                 if cell == '0':
                     result = max(result, rowhits + colhits[j])
         return result
+        
         """
         if not grid:
             return 0
