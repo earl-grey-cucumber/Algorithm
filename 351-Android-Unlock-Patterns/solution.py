921f6b04ddae4bcd05d64c65082307508e97e2f2
didiwuwu <tinadiwu@gmail.com>
2016-09-18 00:21:29 -0700

    [351. Android Unlock Patterns][Accepted]committed by LeetCode Extension

--- 351-Android-Unlock-Patterns/solution.py
+++ 351-Android-Unlock-Patterns/solution.py
@@ -1,10 +1,34 @@
 class Solution(object):
     def numberOfPatterns(self, m, n):
-        """
-        :type m: int
-        :type n: int
-        :rtype: int
-        """
+        def dfs(visited, skip, cur, remain):
+            if remain < 0:
+                return 0
+            if remain == 0:
+                return 1
+            visited[cur] = True
+            count = 0
+            for i in range(1, 10):
+                if not visited[i] and (skip[cur][i] == 0 or visited[skip[cur][i]]):
+                    count += dfs(visited, skip, i, remain - 1)
+            visited[cur] = False
+            return count
+            
+        skip = [[0 for i in range(10)] for j in range(10)]
+        skip[1][3] = skip[3][1] = 2
+        skip[1][7] = skip[7][1] = 4
+        skip[3][9] = skip[9][3] = 6
+        skip[7][9] = skip[9][7] = 8
+        skip[1][9] = skip[9][1] = skip[2][8] = skip[8][2] = skip[3][7] = skip[7][3] = skip[4][6] = skip[6][4] = 5
+        visited = [False] * 10
+        result = 0
+        for i in range(m, n + 1):
+            result += dfs(visited, skip, 1, i - 1) * 4
+            result += dfs(visited, skip, 2, i - 1) * 4
+            result += dfs(visited, skip, 5, i - 1)
+        return result
+        
+    """
+    def numberOfPatterns(self, m, n):
         visited = [False for i in range(9)]
         count = 0
         for i in range(m, n + 1):
@@ -36,3 +60,4 @@ class Solution(object):
         if (last % 3 != cur % 3) and (last / 3 != cur / 3):
             return True
         return visited[mid]
+    """
