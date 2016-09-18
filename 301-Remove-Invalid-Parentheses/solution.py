59fdd9fe31979f470f51ad38f687f37215969355
didiwuwu <tinadiwu@gmail.com>
2016-09-17 18:14:49 -0700

    [301. Remove Invalid Parentheses][Accepted]committed by LeetCode Extension

--- 301-Remove-Invalid-Parentheses/solution.py
+++ 301-Remove-Invalid-Parentheses/solution.py
@@ -4,6 +4,35 @@ class Solution(object):
         :type s: str
         :rtype: List[str]
         """
+        """
+        def is_valid(cur):
+            left, right = 0, 0
+            for i in range(len(cur)):
+                if cur[i] == '(':
+                    left += 1
+                elif cur[i] == ')':
+                    right += 1
+                if left < right:
+                    return False
+                return left == right
+        result, queue, visited = [], [s], set()
+        visited.add(s)
+        found = False
+        while queue:
+            cur = queue.pop()
+            if is_valid(cur):
+                found = True
+                result.append(cur)
+            if found:
+                continue
+            for i in range(len(cur)):
+                if cur[i] == '(' or cur[i] == ')':
+                    cand = cur[0:i] + cur[i + 1:]
+                    if cand not in visited:
+                        visited.add(cand)
+                        queue.append(cand)
+        return result
+        """
         result, queue, visited = [], [s], set()
         visited.add(s)
         found = False
@@ -32,3 +61,6 @@ class Solution(object):
             if left < right:
                 return False
         return left == right
+
+        
+            
