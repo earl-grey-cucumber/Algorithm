81e7094db8da6ddc504fe76ef3fb09cc6ff64530
didiwuwu <tinadiwu@gmail.com>
2016-09-17 21:33:56 -0700

    [361. Bomb Enemy][Accepted]committed by LeetCode Extension

--- 361-Bomb-Enemy/solution.py
+++ 361-Bomb-Enemy/solution.py
@@ -8,56 +8,48 @@ class Solution(object):
         if not grid:
             return 0
         m, n = len(grid), len(grid[0])
-        result, rowhit, colhit = 0, 0, [0 for i in range(n)]
+        result, rowhit, colhit = 0, 0, [0] * n
         for i in range(m):
             for j in range(n):
                 if j == 0 or grid[i][j - 1] == 'W':
                     rowhit = 0
                     k = j
                     while k < n and grid[i][k] != 'W':
-                        if grid[i][k] == 'E':
-                            rowhit += 1
+                        #if grid[i][k] == 'E':
+                        rowhit += grid[i][k] == 'E'
                         k += 1
                 if i == 0 or grid[i - 1][j] == 'W':
                     colhit[j] == 0
                     k = i
                     while k < m and grid[k][j] != 'W':
-                        if grid[k][j] == 'E':
-                            colhit[j] += 1
+                        #if grid[k][j] == 'E':
+                        colhit[j] += grid[k][j] == 'E'
                         k += 1
                 if grid[i][j] == '0':
                     result = max(result, rowhit + colhit[j])
         return result
         """
-        row_size = len(grid)
-        col_size = len(grid[0]) if row_size else 0
-        max_killed = 0
-        row_e, col_e = 0, [0] * col_size
-        for ri in xrange(row_size):
-            for ci in xrange(col_size):
-
-               # no need to re-calculate enemy if there is no wall behind unless it's a col=0
-                if ci == 0 or grid[ri][ci-1] == 'W':
-                    row_e = 0
-                    for k in xrange(ci, col_size):
-                        if grid[ri][k] == 'W': break
-                        row_e += grid[ri][k] == 'E'
-
-                # no need to re-calculate enemy if there is no wall behind unless it's a row=0
-                # if wall is behind, start couting enemy
-                if ri == 0 or grid[ri-1][ci] == 'W':
-                    col_e[ci] = 0
-                    for k in xrange(ri, row_size):
-                        if grid[k][ci] == 'W': break
-                        col_e[ci] += grid[k][ci] == 'E'
-
-                # empty spot, check max_killed
-                if grid[ri][ci] == '0':
-                    max_killed = max(max_killed, row_e + col_e[ci])
-
-        return max_killed
-           
-      
+        if not grid: return 0
+        m, n = len(grid), len(grid[0])
+        result = 0
+        colhits = [0] * n
+        for i, row in enumerate(grid):
+            for j, cell in enumerate(row):
+                if j == 0 or row[j-1] == 'W':
+                    rowhits = 0
+                    k = j
+                    while k < n and row[k] != 'W':
+                        rowhits += row[k] == 'E'
+                        k += 1
+                if i == 0 or grid[i-1][j] == 'W':
+                    colhits[j] = 0
+                    k = i
+                    while k < m and grid[k][j] != 'W':
+                        colhits[j] += grid[k][j] == 'E'
+                        k += 1
+                if cell == '0':
+                    result = max(result, rowhits + colhits[j])
+        return result
         """
         if not grid:
             return 0
