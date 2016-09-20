fe754cf3ebb6886cd1b101d4a3135455992688a6
didiwuwu <tinadiwu@gmail.com>
2016-09-19 23:07:15 -0700

    [221. Maximal Square][Accepted]committed by LeetCode Extension

---
+++ 221-Maximal-Square/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maximalSquare(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        if not matrix:
+            return 0
+        m, n = len(matrix), len(matrix[0])
+        dp = [[0 for i in range(n + 1)] for j in range(m + 1)]
+        result = 0
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if matrix[i - 1][j - 1] == '1':
+                    dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
+                    result = max(result, dp[i][j])
+        return result * result
+                
