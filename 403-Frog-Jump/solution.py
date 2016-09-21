ac8a94c876fa465e5145145dee7bf1febc0c9190
didiwuwu <tinadiwu@gmail.com>
2016-09-20 23:52:07 -0700

    [403. Frog Jump][Accepted]committed by LeetCode Extension

--- 403-Frog-Jump/solution.py
+++ 403-Frog-Jump/solution.py
@@ -1,20 +1,13 @@
 class Solution(object):
     def canCross(self, stones):
-        """
-        :type stones: List[int]
-        :rtype: bool
-        """
+        lookup = {}
+        for k, v in enumerate(stones):
+            lookup[v] = k
         dp = [False for _ in xrange(len(stones))]
         dp[0] = True
-        if stones[1] - stones[0] == 1:
-            dp[1] = True
-        #if len(stones) == 2:
-        #   return dp[-1]
-        for i in xrange(2, len(stones)):
-            for j in reversed(xrange(i)):
-                if stones[i] - stones[j] > j + 1:
-                    break
-                if dp[j] and ((stones[i] - stones[j]) in [j-1, j, j+1]):
-                    dp[i] = True
-                    break
+        for i in xrange(len(stones)):
+            if dp[i]:
+                for k in (i-1, i, i+1):
+                    if stones[i] + k in lookup:
+                        dp[lookup[stones[i] + k]] = True
         return dp[-1]
