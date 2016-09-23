74641918376c6447cf58ff91d8fcf43c84b3362b
didiwuwu <tinadiwu@gmail.com>
2016-09-23 11:27:04 -0700

    [390. Elimination Game][Accepted]committed by LeetCode Extension

---
+++ 390-Elimination-Game/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def lastRemaining(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        start, step, direction = 1, 2, 1
+        while n > 1:
+            start += direction * (step * (n/2) - step/2)
+            n /= 2
+            step *= 2
+            direction *= -1
+        return start
