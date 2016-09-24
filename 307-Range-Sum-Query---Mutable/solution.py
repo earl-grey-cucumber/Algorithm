5f5ca6c46ddec88650eab835dcba70197e565819
didiwuwu <tinadiwu@gmail.com>
2016-09-24 14:29:35 -0700

    [307. Range Sum Query - Mutable][Accepted]committed by LeetCode Extension

---
+++ 307-Range-Sum-Query---Mutable/solution.py
@@ -0,0 +1,31 @@
+class NumArray(object):
+    def __init__(self, nums):
+        """
+        initialize your data structure here.
+        :type nums: List[int]
+        """
+        
+
+    def update(self, i, val):
+        """
+        :type i: int
+        :type val: int
+        :rtype: int
+        """
+        
+
+    def sumRange(self, i, j):
+        """
+        sum of elements nums[i..j], inclusive.
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        
+
+
+# Your NumArray object will be instantiated and called as such:
+# numArray = NumArray(nums)
+# numArray.sumRange(0, 1)
+# numArray.update(1, 10)
+# numArray.sumRange(1, 2)
