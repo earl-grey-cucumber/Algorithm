315ca40af83f8f1e2d61db10e75908d3de48261a
didiwuwu <tinadiwu@gmail.com>
2016-09-24 12:21:26 -0700

    [164. Maximum Gap][Accepted]committed by LeetCode Extension

--- 164-Maximum-Gap/solution.py
+++ 164-Maximum-Gap/solution.py
@@ -1,32 +1,32 @@
+import math
 class Solution(object):
     def maximumGap(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        def counting_sort(nums, base, n):
-            count = [0] * 10
-            output = [0] * n
-            for i in range(n):
-                count[(nums[i] / base) % 10] += 1
-            for i in range(1, 10):
-                count[i] += count[i - 1]
-            for i in range(n - 1, -1, -1):
-                output[count[(nums[i] / base) % 10] - 1] = nums[i]
-                count[(nums[i] / base) % 10] -= 1
-            for i in range(n):
-                nums[i] = output[i]
-        if len(nums) < 2:
+        n = len(nums)
+        if n < 2:
             return 0
-        max_val = nums[0]
+        max_gap = float("-inf")
+        max_val, min_val = nums[0], nums[0]
         for num in nums:
             max_val = max(max_val, num)
-        base,n = 1, len(nums)
-        while max_val > 0:
-            counting_sort(nums, base, n)
-            base *= 10
-            max_val /= 10
-        max_gap = float("-inf")
-        for i in range(1, n):
-            max_gap = max(max_gap, nums[i] - nums[i - 1])
+            min_val = min(min_val, num)
+        gap = int(math.ceil(float(max_val - min_val) / (n - 1)))
+        start = [max_val + 1] * (n - 1)
+        end = [min_val - 1] * (n - 1)
+        for i in range(n):
+            if nums[i] == max_val or nums[i] == min_val:
+                continue
+            id = (nums[i] - min_val) / gap
+            start[id] = min(start[id], nums[i])
+            end[id] = max(end[id], nums[i])
+        pre = min_val
+        for i in range(n - 1):
+            if start[i] == max_val + 1:
+                continue
+            max_gap = max(max_gap, start[i] - pre)
+            pre = end[i]
+        max_gap = max(max_gap, max_val - pre)
         return max_gap
