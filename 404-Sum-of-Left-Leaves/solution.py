f0db9130647dd2b7a1daa7c40523216cc953b3be
didiwuwu <tinadiwu@gmail.com>
2016-09-25 13:26:00 -0700

    [404. Sum of Left Leaves][Accepted]committed by LeetCode Extension

--- 404-Sum-of-Left-Leaves/solution.py
+++ 404-Sum-of-Left-Leaves/solution.py
@@ -11,16 +11,9 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        if not root:
+        if root == None:
             return 0
-        sum = [0]
-        def helper(cur, is_left, sum):
-            if not cur:
-                return 0
-            if is_left and not cur.left and not cur.right:
-                sum[0] += cur.val
-            helper(cur.left, True, sum)
-            helper(cur.right, False, sum)
-        helper(root.left, True, sum)
-        helper(root.right, False, sum)
-        return sum[0]
+        if root.left and not root.left.left and not root.left.right:
+            return root.left.val + self.sumOfLeftLeaves(root.right)
+        else:       # isn't leave
+            return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
