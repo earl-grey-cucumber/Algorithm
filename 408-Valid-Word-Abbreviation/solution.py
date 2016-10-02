f462dfddfe0a79e14f95274875e99874ebea4f1f
didiwuwu <tinadiwu@gmail.com>
2016-10-02 11:11:02 -0700

    [408. Valid Word Abbreviation][Accepted]committed by LeetCode Extension

---
+++ 408-Valid-Word-Abbreviation/solution.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def validWordAbbreviation(self, word, abbr):
+        """
+        :type word: str
+        :type abbr: str
+        :rtype: bool
+        """
+        if not word and not abbr:
+            return True
+        if not word or not abbr:
+            return False
+        i, n, j = 0, len(abbr), 0
+        while i < n and j < len(word):
+            if abbr[i].isdigit():
+                num = 0
+                if abbr[i] == '0':
+                    return False
+                while i < n and abbr[i].isdigit():
+                    num = num * 10 + ord(abbr[i]) - ord('0')
+                    i += 1
+                j += num
+            else:
+                if abbr[i] != word[j]:
+                    return False
+                i += 1
+                j += 1
+        return i == n and j == len(word)
+                
+            
