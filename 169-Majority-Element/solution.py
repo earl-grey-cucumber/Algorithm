d4b2ff5c771c9dae1e754bcdb58e55bae2122629
didiwuwu <tinadiwu@gmail.com>
2016-10-03 23:47:15 -0700

    [169. Majority Element][Accepted]committed by LeetCode Extension

--- 169-Majority-Element/solution.py
+++ 169-Majority-Element/solution.py
@@ -1,12 +1,7 @@
 class Solution(object):
     def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
         mask =    0xFFFFFFFF
         max_int = 0x7FFFFFFF
-
         n = 32
         bits = [0 for i in range(n)]
         for num in nums:
@@ -18,11 +13,3 @@ class Solution(object):
             if bits[i] * 2 > len(nums): 
                 result += (1 << i)
         return result if result <= max_int else ~(result ^ mask)
-        """
-        leftmost = (result >> 31) & 1
-        print(leftmost)
-        result = (result & 0x7fffffff)
-        print(result)
-        result |= (leftmost << 31)
-        """
-        #return result if result != 2147483648 else -2147483648
